d57345b5d42db625e0b0ca1dbc637beefa7024d7
jwyc <janis.wy.chan@gmail.com>
2021-12-06 22:34:54 -0800

    identified issue with var naming issue userResults and userRequest

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 const randomFolks = document.querySelector(".random-peeps");
 
 const getData = async function(){
-   const usersResults = await fetch ("https://randomuser.me/api?results=5");
-   const data = await usersResults.json();
+   const usersRequests = await fetch ("https://randomuser.me/api?results=5");
+   const data = await usersRequest.json();
    console.log(data);
+   displayUsers(userResults);
+   
 }
 
 getData();
 
-const displayUsers = function(usersResults){
+const displayUsers = function(userResults){
    randomFolks.innerHTML ="";
-   for (const user in usersResults){
-      const country = data.location.country;
-      const name = data.name.first;
-      const imgURL = data.picture.medium;
+   for (let user in usersResults){
+      const country = user.location.country;
+      const name = user.name.first;
+      const imgURL = user.picture.medium;
+      document.createElement("userDiv");
+      userDiv.innerHTML = `
+         <h3>${name}</h3>
+         <p>${country}</p>
+         <img src ${imgURL} alt= "User avatar"/>
+      `;
+      randomFolks.append(userDiv);
    }
 
 };
