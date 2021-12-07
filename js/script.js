66083c9517b6ab76c400df025015a64884bc9c50
jwyc <janis.wy.chan@gmail.com>
2021-12-06 21:16:04 -0800

    created async func to connect to randomuser api, call to confirm object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function(){
+   const usersResults = await fetch ("https://randomuser.me/api?results=5");
+   const data = await usersResults.json();
+   console.log(data);
+}
+
+getData();
+
