73da1012b1174d012f6154b584c57907da32f8de
jwyc <janis.wy.chan@gmail.com>
2021-12-08 22:33:27 -0800

    captured select element with #users id, add change eventlistener to capture selections, ran that selection through getData call,  modified getdata function with paramater numUsers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function(){
-   const usersRequests = await fetch ("https://randomuser.me/api?results=5");
+const getData = async function(numUsers){
+   const usersRequests = await fetch (`https://randomuser.me/api?results=${numUsers}`);
    const data = await usersRequests.json();
    const userResults = data.results;
    console.log(userResults);
+
    displayUsers(userResults);
    
 }
 
-getData();
+getData(1);
 
 const displayUsers = function(userResults){
    randomFolks.innerHTML ="";
@@ -17,7 +19,7 @@ const displayUsers = function(userResults){
       const country = user.location.country;
       const name = user.name.first;
       const imgURL = user.picture.medium;
-      const userDiv = document.createElement("Div");
+      const userDiv = document.createElement("div");
       userDiv.innerHTML = `
          <h3>${name}</h3>
          <p>${country}</p>
@@ -27,3 +29,9 @@ const displayUsers = function(userResults){
    }
 
 };
+
+selectUserNumber.addEventListener("change", function(e){
+   const numUsers=e.target.value;
+
+   getData(numUsers);
+});
